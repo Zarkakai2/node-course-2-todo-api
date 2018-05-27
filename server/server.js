6463dcbd151cddb2a61b641e3ad58596bc3a2386
Aurelien GAMET <aurelien.gamet@cgi.com>
2018-05-27 14:05:02 +0200

    Added ADD /users

--- server/server.js
+++ server/server.js
@@ -80,6 +80,19 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    var user = new User({ email: body.email, password: body.password });
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(port, () => {
     console.log('Started on port ' + port)
 });
